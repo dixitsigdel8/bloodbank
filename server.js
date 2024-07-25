73fc94f9fd26536a90128eb90acf4f04cfe0e3e2
unknown <dixitsigdel8@gmail.com>
2024-07-25 22:11:10 +1000

    project setup

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const colors = require('colors');
+const morgan = require('morgan');
+const cors = require('cors');
+
+//dot config
+dotenv.config()
 
 //rest object 
 
 const app = express();
 
+//middlewares
+app.use(express.json())
+app.use(cors());
+app.use(morgan('dev'))
+
+
 //port 
-const PORT = 8000
+const PORT = process.env.PORT || 8080;
 
 //listen
 app.listen(`${PORT}`,()=>{
-    console.log(`Server is running on ${PORT} Port`)
+    console.log(`Server running in ${process.env.DEV_MODE} on ${PORT} Port`)
 })
